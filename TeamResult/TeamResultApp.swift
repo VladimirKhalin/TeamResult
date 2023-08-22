a15d611eeae8fb8c7a93808aefde1529544441eb
VK <art.nebar@mail.ru>
2023-08-22 13:30:27 +0300

    Initial Commit

---
+++ TeamResult/TeamResultApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamResultApp.swift
+//  TeamResult
+//
+//  Created by Vladimir Khalin on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TeamResultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
